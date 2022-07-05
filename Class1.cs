ba7c64d98c4352b2e425b1e0f39d02b9bf9d27a3
samrawitnebiyou <85891676+samrawitnebiyou@users.noreply.github.com>
2022-07-05 20:40:06 +0300

    Add files via upload

---
+++ Class1.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace labform1
+{
+    class item
+    {
+        public int Number { get; set; }
+
+        public string Date { get; set; }
+
+        public int Inventorynum { get; set; }
+
+        public string Objectname { get; set; }
+        public int Count { get; set; }
+
+        public double Price { get; set; }
+
+        
+
+        public void Save()
+        {
+            Console.WriteLine("SAVED");
+        }
+    }
+}
