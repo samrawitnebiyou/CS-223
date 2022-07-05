3270c1a419b2bd8f3bfce03e71278201d3436361
samrawitnebiyou <85891676+samrawitnebiyou@users.noreply.github.com>
2022-07-05 20:41:05 +0300

    Add files via upload

---
+++ Form1.Designer.cs
@@ -0,0 +1,214 @@
+﻿namespace labform1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.txt_number = new System.Windows.Forms.TextBox();
+            this.txt_inventorynumber = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.txt_date = new System.Windows.Forms.DateTimePicker();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.txt_objectname = new System.Windows.Forms.TextBox();
+            this.txt_price = new System.Windows.Forms.TextBox();
+            this.txt_count = new System.Windows.Forms.TextBox();
+            this.label6 = new System.Windows.Forms.Label();
+            this.btn_cancel = new System.Windows.Forms.Button();
+            this.btn_additem = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // txt_number
+            // 
+            this.txt_number.Location = new System.Drawing.Point(26, 49);
+            this.txt_number.Name = "txt_number";
+            this.txt_number.Size = new System.Drawing.Size(100, 20);
+            this.txt_number.TabIndex = 1;
+            // 
+            // txt_inventorynumber
+            // 
+            this.txt_inventorynumber.Location = new System.Drawing.Point(413, 52);
+            this.txt_inventorynumber.Name = "txt_inventorynumber";
+            this.txt_inventorynumber.Size = new System.Drawing.Size(257, 20);
+            this.txt_inventorynumber.TabIndex = 3;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(22, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(85, 24);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Number";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(172, 9);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(52, 24);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "Date";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(436, 9);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(173, 24);
+            this.label3.TabIndex = 5;
+            this.label3.Text = "Inventory number";
+            // 
+            // txt_date
+            // 
+            this.txt_date.Location = new System.Drawing.Point(162, 49);
+            this.txt_date.Name = "txt_date";
+            this.txt_date.Size = new System.Drawing.Size(200, 20);
+            this.txt_date.TabIndex = 6;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.Location = new System.Drawing.Point(22, 96);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(132, 24);
+            this.label4.TabIndex = 8;
+            this.label4.Text = "Object Name";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label5.Location = new System.Drawing.Point(22, 179);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(65, 24);
+            this.label5.TabIndex = 9;
+            this.label5.Text = "Count";
+            // 
+            // txt_objectname
+            // 
+            this.txt_objectname.Location = new System.Drawing.Point(26, 140);
+            this.txt_objectname.Name = "txt_objectname";
+            this.txt_objectname.Size = new System.Drawing.Size(336, 20);
+            this.txt_objectname.TabIndex = 10;
+            // 
+            // txt_price
+            // 
+            this.txt_price.Location = new System.Drawing.Point(242, 215);
+            this.txt_price.Name = "txt_price";
+            this.txt_price.Size = new System.Drawing.Size(100, 20);
+            this.txt_price.TabIndex = 11;
+            this.txt_price.TextChanged += new System.EventHandler(this.textBox2_TextChanged);
+            // 
+            // txt_count
+            // 
+            this.txt_count.Location = new System.Drawing.Point(26, 215);
+            this.txt_count.Name = "txt_count";
+            this.txt_count.Size = new System.Drawing.Size(198, 20);
+            this.txt_count.TabIndex = 12;
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label6.Location = new System.Drawing.Point(238, 179);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(58, 24);
+            this.label6.TabIndex = 13;
+            this.label6.Text = "Price";
+            // 
+            // btn_cancel
+            // 
+            this.btn_cancel.Location = new System.Drawing.Point(367, 284);
+            this.btn_cancel.Name = "btn_cancel";
+            this.btn_cancel.Size = new System.Drawing.Size(75, 23);
+            this.btn_cancel.TabIndex = 14;
+            this.btn_cancel.Text = "Cancel";
+            this.btn_cancel.UseVisualStyleBackColor = true;
+            this.btn_cancel.Click += new System.EventHandler(this.btn_cancel_Click);
+            // 
+            // btn_additem
+            // 
+            this.btn_additem.Location = new System.Drawing.Point(226, 284);
+            this.btn_additem.Name = "btn_additem";
+            this.btn_additem.Size = new System.Drawing.Size(75, 23);
+            this.btn_additem.TabIndex = 15;
+            this.btn_additem.Text = "Add";
+            this.btn_additem.UseVisualStyleBackColor = true;
+            this.btn_additem.Click += new System.EventHandler(this.btn_additem_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.btn_additem);
+            this.Controls.Add(this.btn_cancel);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.txt_count);
+            this.Controls.Add(this.txt_price);
+            this.Controls.Add(this.txt_objectname);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.txt_date);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.txt_inventorynumber);
+            this.Controls.Add(this.txt_number);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Add Inventory";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.TextBox txt_number;
+        private System.Windows.Forms.TextBox txt_inventorynumber;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.DateTimePicker txt_date;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.TextBox txt_objectname;
+        private System.Windows.Forms.TextBox txt_price;
+        private System.Windows.Forms.TextBox txt_count;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Button btn_cancel;
+        private System.Windows.Forms.Button btn_additem;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace labform1
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+              
+
+        }
+
+        private void comboBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox2_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btn_cancel_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+
+        private void btn_additem_Click(object sender, EventArgs e)
+        {
+            item stud = new item();
+            stud.Number = int.Parse(txt_number.Text);
+            stud.Date = txt_date.Text;
+            stud.Inventorynum = int.Parse(txt_inventorynumber.Text);
+            stud.Objectname = txt_objectname.Text;
+            stud.Count = int.Parse(txt_count.Text);
+            stud.Price = int.Parse(txt_price.Text);
+            stud.Save();
+            MessageBox.Show("SAVED");
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
