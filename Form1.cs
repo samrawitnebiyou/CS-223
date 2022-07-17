4a8971f8f63eed19fd422aa7268742770d8143f8
samrawitnebiyou <85891676+samrawitnebiyou@users.noreply.github.com>
2022-07-17 22:44:12 +0300

    Update Form1.cs
    Validation of input

--- Form1.cs
+++ Form1.cs
@@ -7,19 +7,22 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.Text.RegularExpressions;
 
 namespace labform1
 {
     public partial class Form1 : Form
     {
+
         public Form1()
         {
+
             InitializeComponent();
         }
 
         private void button1_Click(object sender, EventArgs e)
         {
-              
+
 
         }
 
@@ -40,18 +43,75 @@ namespace labform1
 
         private void btn_additem_Click(object sender, EventArgs e)
         {
-            item stud = new item();
-            stud.Number = int.Parse(txt_number.Text);
-            stud.Date = txt_date.Text;
-            stud.Inventorynum = int.Parse(txt_inventorynumber.Text);
-            stud.Objectname = txt_objectname.Text;
-            stud.Count = int.Parse(txt_count.Text);
-            stud.Price = int.Parse(txt_price.Text);
-            stud.Save();
-            MessageBox.Show("SAVED");
+            Boolean isVal = true;
+            Regex re = new Regex(@"^[0-9]{5}$");
+            if (!re.IsMatch(txt_inventorynumber.Text))
+            {
+
+                errorProvider1.SetError(txt_inventorynumber, "invalid inventory number!");
+                isVal = false;
+
+            }
+            else
+            {
+                errorProvider1.Clear();
+            }
+
+            try
+            {
+
+                int.Parse(txt_number.Text);
+                errorProvider3.Clear();
+
+            }
+            catch (Exception)
+            {
+
+                errorProvider3.SetError(txt_number, "invalid number!");
+                isVal = false;
+            }
+            if (string.IsNullOrEmpty(txt_objectname.Text))
+            {
+                errorProvider2.SetError(txt_objectname, "invalid object name!");
+                isVal = false;
+            }
+            else
+            {
+                errorProvider2.Clear();
+            }
+
+            if (isVal)
+            {
+
+                item items = new item();
+                items.Number = int.Parse(txt_number.Text);
+                items.Date = txt_date.Text;
+                items.Inventorynum = int.Parse(txt_inventorynumber.Text);
+                items.Objectname = txt_objectname.Text;
+                items.Count = int.Parse(txt_count.Text);
+                items.Price = double.Parse(txt_price.Text);
+                items.Save();
+                dataGridView1.DataSource = null;
+                dataGridView1.DataSource = item.getAll();
+
+            }
+            ;
+
+
+
         }
 
         private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void txt_number_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void txt_objectname_TextChanged(object sender, EventArgs e)
         {
 
         }
