ef3acd94ca33111f87af9c769e74ecfd2c29b7fc
samrawitnebiyou <85891676+samrawitnebiyou@users.noreply.github.com>
2022-07-29 10:15:02 +0300

    Update Program.cs
    app running from login page

--- Program.cs
+++ Program.cs
@@ -9,6 +9,7 @@ namespace labform1
     internal static class Program
     {
         /// <summary>
+        /// 
         /// The main entry point for the application.
         /// </summary>
         [STAThread]
@@ -16,7 +17,7 @@ namespace labform1
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new Form1());
+            Application.Run(new Form2());
         }
     }
 }
