5a3a6dd03193474a3f8310f1c2df730222d98258
samrawitnebiyou <85891676+samrawitnebiyou@users.noreply.github.com>
2022-08-09 21:06:34 +0300

    Add files via upload
    Display All

---
+++ Display.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace labform1
+{
+    public partial class Display : Form
+    {
+        public Display()
+        {
+            InitializeComponent();
+            item.getAll();
+           
+            dataGridView1.DataSource = null;
+            dataGridView1.DataSource = item.getAll();
+
+        }
+
+        private void Display_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void dataGridView1_CellContentClick(object sender, DataGridViewCellEventArgs e)
+
+        {
+
+        }
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
